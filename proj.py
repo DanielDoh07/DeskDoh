8f0b219dfbb405e9ab74167273462cdc7cc8d783
DanielDoh07 <97704814+DanielDoh07@users.noreply.github.com>
2022-05-22 19:32:18 -0500

    first push. having errors with query

---
+++ proj.py
@@ -0,0 +1,83 @@
+import pyttsx3
+import speech_recognition as sr
+import wikipedia
+import pyjokes
+import random
+import requests
+import datetime
+import os
+import pyaudio
+import youtube_dl #yt
+import webbrowser
+
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+
+def speak(clip):
+    engine.say(clip)
+    engine.runAndWait()
+
+def audiotranslate():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=.5)
+        print("Listening...")
+        audio = r.listen(source)
+    try:
+        cmnd = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        print("Sorry, I didn't quite catch that")
+        cmnd = audiotranslate()
+        cmnd = cmnd.lower()
+    return cmnd
+
+def personSearch(name):
+    pass
+
+def sendEmail():
+    pass
+
+def openWEB(cmnd):
+    cmnd = cmnd.lower()
+    s = cmnd.find("open") + 5
+    return cmnd[s:]
+
+
+
+
+
+
+
+
+
+while True:
+    query = audiotranslate()
+    print(query)
+    if "quit" in query:
+        speak("ok")
+        break
+    elif "stop" in query:
+        speak("ok")
+        break
+    elif "open" in query:
+        speak("open")
+        print(openWEB(query))
+
+
+# def main():
+#     run = True
+#     while run == True:
+#         query = audiotranslate()
+#         if "stop" or "thanks" or "bye" or "quit" in query:
+#             print(query)
+#             run == False
+#         elif "open youtube" in query:
+#             speak("Opening youtube")
+
+
+
+# if __name__ == "__main__":
+#     main()
