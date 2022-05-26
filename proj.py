4a4d6d98dc2e4ee439f394934936a813e9ee3500
DanielDoh07 <97704814+DanielDoh07@users.noreply.github.com>
2022-05-26 18:13:20 -0500

    Translated my project into a class, so it can be used for OOP.

--- proj.py
+++ proj.py
@@ -7,8 +7,9 @@ import requests
 import datetime
 import os
 import pyaudio
-import youtube_dl #yt
 import webbrowser
+import smtplib
+
 
 
 engine = pyttsx3.init("sapi5")
@@ -19,6 +20,13 @@ engine.setProperty("voice", voices[1].id)
 def speak(clip):
     engine.say(clip)
     engine.runAndWait()
+
+def telljoke():
+    speak(pyjokes.get_joke())
+
+def time():
+    pass
+
 
 def audiotranslate():
     r = sr.Recognizer()
@@ -34,11 +42,26 @@ def audiotranslate():
         cmnd = cmnd.lower()
     return cmnd
 
+def furtherassistance():
+    speak("Do you require further assistance")
+    response = audiotranslate()
+    if response == "yes" or response == "yeah":
+        return 1
+    elif response == "no":
+        return 2
+    else:
+        speak("Sorry, I didn't quite catch that. Please say yes or no")
+        furtherassistance()
+
 def personSearch(name):
     pass
 
 def sendEmail():
-    pass
+    serv = smtplib.SMTP("smtp.gmail.com")
+    userlogin = input("Please input your user login in the following format: username@gmail.com")
+    userpassword = input("Please input your email's password")
+    sent_from = userlogin
+    to
 
 def openWEB(cmnd):
     cmnd = cmnd.lower()
@@ -47,36 +70,43 @@ def openWEB(cmnd):
     webbrowser.open(url)
 
 
+def main():
+    f = open("emailf.txt", "a")
+    f.write("Hello")
+    f.close()
+    f = open("emailf.txt")
+    print(f.read())
+    f.close()
+    # while True:
+    #     query = audiotranslate()
+    #     print(query)
+    #     if "quit" in query:
+    #         speak("goodbye")
+    #         break
+    #     elif "stop" in query:
+    #         speak("goodbye")
+    #         break
+    #     elif "bye" in query:
+    #         speak("goodbye")
+    #         break
+    #     elif "open" in query:
+    #         speak("on it")
+    #         openWEB(query)
+    #         if furtherassistance() == 1:
+    #             pass
+    #         else:
+    #             break
+    #     elif "who is" in query:
+    #         speak("searching")
+    #     elif "joke" in query:
+    #         telljoke()
+    #         speak("Would you like another joke?")
 
-while True:
-    query = audiotranslate()
-    print(query)
-    if "quit" in query:
-        speak("ok")
-        break
-    elif "stop" in query:
-        speak("ok")
-        break
-    elif "open" in query:
-        speak("on it")
-        print(openWEB(query))
-    elif "who is" in query:
-        speak("searching")
 
 
 
 
-# def main():
-#     run = True
-#     while run == True:
-#         query = audiotranslate()
-#         if "stop" or "thanks" or "bye" or "quit" in query:
-#             print(query)
-#             run == False
-#         elif "open youtube" in query:
-#             speak("Opening youtube")
 
 
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
